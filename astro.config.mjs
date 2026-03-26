501ea87b5bd3deea07edbcfaf076e56f9857b7fd
Luis Gerardo Salazar Aguilar <luissalazar@MacBook-Air-de-Luis.local>
2026-03-26 14:07:37 -0600

    ayudando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'static', // 👈 IMPORTANTE
+  site: 'https://studio.luissalazar.com.mx', // 🔴 CORREGIDO
+  output: 'static',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
